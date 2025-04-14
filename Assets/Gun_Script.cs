040692cb63bd0128282dc3e20aa3e90b510fe1bb
BigChugJug <j.thompson92103@gmail.com>
2025-04-14 15:41:35 -0500

    Justin got the script for the gun

--- Assets/Gun_Script.cs
+++ Assets/Gun_Script.cs
@@ -1,6 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun_Script
+public class Gun_Script : MonoBehaviour
 {
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 20f;
+
+    public void Shoot()
+    {
+        // Create the bullet at the firePoint's position and rotation
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
+        // Get the Rigidbody component from the bullet to apply force
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            // Add force to the bullet to make it move
+            rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+        }
+            
+    }
     
 }
+
