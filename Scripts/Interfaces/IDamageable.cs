fa34197364a7e3a424b712d1825042007e631565
BigChugJug <j.thompson92103@gmail.com>
2025-04-24 09:05:18 -0500

    Revert "Justin added scripts for ai and weapons"
    This reverts commit 00daeca94a95630e084d53c919ead56602e48aed.

--- Scripts/Interfaces/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageable
-{
-    void TakeDamage(int damageAmount); //Take damage depending on what the bullet value is
-}
-
--- Scripts/Interfaces/IWeapon.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IWeapon
-{
-    void Fire(); // Method that will be implemented differently for each weapon type
-}
